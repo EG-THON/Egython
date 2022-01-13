dcd10b1eed4bb7a9b5b60d52f641229e1fa5a4ad
EG-THON <97476166+EG-THON@users.noreply.github.com>
2022-01-13 16:14:41 +0200

    Delete userbot/assistant directory

--- userbot/assistant/_bot.py
+++
@@ -1,5 +0,0 @@
-from . import *
-
-@asst_cmd("alive")
-async def _(e):
-    await asst.send_file(e.chat_id, asst_p, caption=asst_c)
--- userbot/assistant/kimo.py
+++
@@ -1,8 +0,0 @@
-#  Egython - USERBOT
-#  TELE - @Egython
-
-from . import Devt
-
-@asst_cmd("سورس")
-async def _(kimo):
-    await kimo.reply(Devt)
